127786086bb0ed80bad95d9fd73aa03c05943fbe
Héctor Villegas <118067595+HEJVR@users.noreply.github.com>
2024-07-24 20:09:33 -0500

    Delete JS directory

--- JS/script.js
+++
@@ -1,5 +0,0 @@
-var typed = new Typed('.typed', {
-    strings: ["Diseñador y desarrollador web en formación. He estado encaminándome en el área del diseño y la programación durante más de 5 años. Me considero una persona polifacética y abierta a aprender constantemente, lo cual me ha permitido adquirir numerosos conocimientos tanto en diseño gráfico como en programación."],
-    typeSpeed: 18
-});
-
